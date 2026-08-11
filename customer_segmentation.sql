9f5bb94ebf55b8b0e109668186d230c4bcf7ddbd
Jens <jenniferk973@outlook.com>
2026-08-11 22:56:12 +0100

    Create customer_orders table for order data

---
+++ customer_segmentation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer_orders (
+  customer_id TEXT not null,
+  order_id TEXT not null,
+  order_date date not null,
+  branch_id INTEGER not null,
+  order_method TEXT not null,
+  order_type TEXT not null,
+  Spend_GDP FLOAT not null,
+  )
